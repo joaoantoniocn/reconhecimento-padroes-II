971d5f86a93f30107cf93f434848b7958755336e
Joao Antonio C. Nunes <joaoantoniocn@gmail.com>
2016-09-18 14:04:04 -0300

    Exercicio 2 base iris FEITO

--- main.m
+++ main.m
@@ -1,26 +1,36 @@
+% Referente a atividade do arquivo aula01.pdf
+
 clear;
 
 % carregar base
 [ matriz, legenda] = lerBase( './base/iris.txt' );
+
 % separar treino e teste
 nameClasses = [cellstr('Iris-setosa') cellstr('Iris-versicolor') cellstr('Iris-virginica')];
 [ treino, legendaTreino, teste, legendaTeste ] =  separaBase( matriz, legenda, 10, 1, nameClasses );
+
 % calcular taxa de acerto usando 1-NN (4 dimensoes)
 resultado = classificarKNN( teste, treino, legendaTeste, legendaTreino );
+
 % plotar grafico das duas primeiras dimensoes do conjunto de treino
 plotarGrafico(treino, legendaTreino, 1, 2, 'Duas primeiras dimensoes no conjunto de treino');
+
 % calcular taxa de acerto usando apenas as duas primeiras dimensoes
 teste2 = [teste(:, 1) teste(:, 2)];
 treino2 = [treino(:, 1) treino(:, 2)];
 resultado2 = classificarKNN( teste2, treino2, legendaTeste, legendaTreino );
+
 % Treinar o PCA no conjunto de treino.
 pca = PCA(treino);
+
 % Projetar conjunto de treino e teste com 2 componentes principais do pca
 projecaoTreino =  projetarPCA( treino, pca, mean(treino) );
 projecaoTeste =  projetarPCA( teste, pca, mean(treino) );
+
 % Gerar o gráfico para o conjunto de treino com as duas dimensões extraídas com PCA, 
 % diferenciando cada classe.
 plotarGrafico(projecaoTreino, legendaTreino, 1, 2, 'Projecao PCA conjunto de treino');
 plotarGrafico(projecaoTeste, legendaTeste, 1, 2, 'Projecao PCA conjunto de teste');
+
 % Calcular a taxa de acerto utilizando as duas características extraídas com PCA.
 resultadoPCA = classificarKNN( projecaoTeste, projecaoTreino, legendaTeste, legendaTreino );
--- projetarPCA.m
+++ projetarPCA.m
@@ -4,18 +4,30 @@ function [ projecao ] = projetarPCA( base, pca, media )
 
     projecao = [];
     
+    % roda cada exemplo da base
     for i = 1 : length(base)
+        
+        % to usando 4 dimensões pq a base Iris tem 4 dimensões
+        % OBS: ajustar o número de dimensões para cada base
         Xi1 = 0; % primeira dimensao projetada
         Xi2 = 0; % segunda dimensao projetada
+        Xi3 = 0;
+        Xi4 = 0;
+        
+        % roda cada dimensão do exemplo i
         for j = 1 : length(base(1,:))
             temp1 = (base(i,j) - media(j)) * pca(j,1);
             temp2 = (base(i,j) - media(j)) * pca(j,2);
+            temp3 = (base(i,j) - media(j)) * pca(j,3);
+            temp4 = (base(i,j) - media(j)) * pca(j,4);
             
             Xi1 = Xi1 + temp1;
             Xi2 = Xi2 + temp2;
+            Xi3 = Xi3 + temp3;
+            Xi4 = Xi4 + temp4;
         end
         
-        projecao = [projecao; [Xi1 Xi2]];
+        projecao = [projecao; [Xi1 Xi2 Xi3 Xi4]];
         
     end    
 
